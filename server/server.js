fe9c60d33760d8ddd0a478402e5d56336e84a305
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2022-02-26 17:50:49 +0200

    Adding more settings

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import Gun from "gun";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(Gun.serve);
+
+app.get("/", (_, res) => {
+  res.status(200).send("> DEBUG: Discord Node is Live");
+});
+
+const server = app.listen(port, () => {
+  console.log(`DEBUG: Discord Node is Listen on PORT:${port}`);
+});
+
+Gun({ web: server });
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -11,4 +11,10 @@ export default createSchema({
   // We name our schema
   name: "default",
   // Then proceed to concatenate our document type
-  // to the ones provi
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    /* Your types here! */
+    userSchema,
+    conversationSchema,
+  ]),
+});
