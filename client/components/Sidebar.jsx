888fad60a453a1f16a8da3c92720f470a70acaff
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2022-02-26 19:37:14 +0200

    Add Styles and creating Sidebar

---
+++ client/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/sidebar.module.css'
+import {useRouter} from 'next/router'
+const Sidebar = () => {
+  return (
+    <div className={styles.wrapper}>Sidebar</div>
+  )
+}
+
+export default Sidebar
